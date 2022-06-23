6ecda13d954b3f5aac6ea1f6cf04034d7e2e4e18
MerchantKhalid88 <mohammadkhr88@gmail;com>
2022-06-23 07:33:00 +0200

    quick read completed

--- Completed/assets/js/main.js
+++ Completed/assets/js/main.js
@@ -57,3 +57,18 @@ formCloseBtn.addEventListener("click", () =>
 window.addEventListener("keyup", (event) => {
   if (event.key === "Escape") searchFormContainer.classList.remove("activated");
 });
+
+// swiper pagination
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: { el: ".swiper-pagination" },
+  breakpoints: {
+    700: { slidesPerView: 2 },
+    1200: { slidesPerView: 3 },
+  },
+});
